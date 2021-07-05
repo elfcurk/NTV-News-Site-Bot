4375438852568e3b8f0487124b8663c52154cd8b
elfcurk <elfcurk@users.noreply.github.com>
2021-07-06 00:30:47 +0300

    Data Extraction with C#
    Extracting News Data from Browser with C# Html Agility Pack.

---
+++ Data Extraction with C#/NTV-News Data Extraction/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace TezNTVHaberCekme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.haberal = new System.Windows.Forms.Button();
+            this.haberler = new System.Windows.Forms.ListBox();
+            this.linkBox = new System.Windows.Forms.ListBox();
+            this.newsBox = new System.Windows.Forms.ListBox();
+            this.headBox = new System.Windows.Forms.ListBox();
+            this.dbKaydet = new System.Windows.Forms.Button();
+            this.veriGet = new System.Windows.Forms.Button();
+            this.listHaber = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.listHaber)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // haberal
+            // 
+            this.haberal.Location = new System.Drawing.Point(155, 678);
+            this.haberal.Name = "haberal";
+            this.haberal.Size = new System.Drawing.Size(253, 29);
+            this.haberal.TabIndex = 0;
+            this.haberal.Text = "Haber Al";
+            this.haberal.UseVisualStyleBackColor = true;
+            this.haberal.Click += new System.EventHandler(this.haberal_Click);
+            // 
+            // haberler
+            // 
+            this.haberler.FormattingEnabled = true;
+            this.haberler.Location = new System.Drawing.Point(12, 12);
+            this.haberler.Name = "haberler";
+            this.haberler.Size = new System.Drawing.Size(536, 186);
+            this.haberler.TabIndex = 1;
+            this.haberler.SelectedIndexChanged += new System.EventHandler(this.haberler_SelectedIndexChanged);
+            // 
+            // linkBox
+            // 
+            this.linkBox.FormattingEnabled = true;
+            this.linkBox.Location = new System.Drawing.Point(12, 204);
+            this.linkBox.Name = "linkBox";
+            this.linkBox.Size = new System.Drawing.Size(536, 173);
+            this.linkBox.TabIndex = 2;
+            // 
+            // newsBox
+            // 
+            this.newsBox.FormattingEnabled = true;
+            this.newsBox.Location = new System.Drawing.Point(554, 12);
+            this.newsBox.Name = "newsBox";
+            this.newsBox.Size = new System.Drawing.Size(491, 186);
+            this.newsBox.TabIndex = 3;
+            // 
+            // headBox
+            // 
+            this.headBox.FormattingEnabled = true;
+            this.headBox.Location = new System.Drawing.Point(554, 204);
+            this.headBox.Name = "headBox";
+            this.headBox.Size = new System.Drawing.Size(491, 173);
+            this.headBox.TabIndex = 4;
+            // 
+            // dbKaydet
+            // 
+            this.dbKaydet.Location = new System.Drawing.Point(427, 679);
+            this.dbKaydet.Name = "dbKaydet";
+            this.dbKaydet.Size = new System.Drawing.Size(221, 28);
+            this.dbKaydet.TabIndex = 5;
+            this.dbKaydet.Text = "Veritabanına Kaydet";
+            this.dbKaydet.UseVisualStyleBackColor = true;
+            this.dbKaydet.Click += new System.EventHandler(this.dbKaydet_Click);
+            // 
+            // veriGet
+            // 
+            this.veriGet.Location = new System.Drawing.Point(691, 679);
+            this.veriGet.Name = "veriGet";
+            this.veriGet.Size = new System.Drawing.Size(221, 28);
+            this.veriGet.TabIndex = 7;
+            this.veriGet.Text = "Veritabanından Verileri Getir";
+            this.veriGet.UseVisualStyleBackColor = true;
+            this.veriGet.Click += new System.EventHandler(this.veriGet_Click);
+            // 
+            // listHaber
+            // 
+            this.listHaber.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.listHaber.Location = new System.Drawing.Point(12, 383);
+            this.listHaber.Name = "listHaber";
+            this.listHaber.Size = new System.Drawing.Size(1033, 262);
+            this.listHaber.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1092, 741);
+            this.Controls.Add(this.listHaber);
+            this.Controls.Add(this.veriGet);
+            this.Controls.Add(this.dbKaydet);
+            this.Controls.Add(this.headBox);
+            this.Controls.Add(this.newsBox);
+            this.Controls.Add(this.linkBox);
+            this.Controls.Add(this.haberler);
+            this.Controls.Add(this.haberal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.listHaber)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button haberal;
+        private System.Windows.Forms.ListBox haberler;
+        private System.Windows.Forms.ListBox linkBox;
+        private System.Windows.Forms.ListBox newsBox;
+        private System.Windows.Forms.ListBox headBox;
+        private System.Windows.Forms.Button dbKaydet;
+        private System.Windows.Forms.Button veriGet;
+        private System.Windows.Forms.DataGridView listHaber;
+    }
+}
+
---
+++ Data Extraction with C#/NTV-News Data Extraction/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using System.Net;
+using System.Data.OleDb;
+namespace TezNTVHaberCekme
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        OleDbConnection con = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\Users\\Sefa\\Documents\\haberdb.accdb;Persist Security Info=True");
+        OleDbDataAdapter adtr;
+        DataTable tablo = new DataTable();
+        private void verileriGetir()
+        {
+            con.Open();
+            OleDbCommand komut = new OleDbCommand();
+            komut.Connection = con;
+            komut.CommandText = ( "Select * From haber" );
+            adtr = new OleDbDataAdapter(komut);
+            adtr.Fill(tablo);
+            listHaber.DataSource = tablo;   
+            con.Close();
+
+        }
+        string[] link=new String[72];
+        string[] content = new String[72];
+        string[] title=new String[72];
+        string[] category=new String[100];
+        int k = 0, l = 0 , m = 0 , n = 0;
+        private void linkGonder(String a)
+        {
+            a = a.Replace("&#39;", " ");
+            a = a.Replace("&#160;", " ");
+            link[k] = a;
+            k++;
+            /*con.Open();
+            OleDbCommand komut = new OleDbCommand();
+            komut.Connection = con;
+            komut.CommandText = ("INSERT INTO haber(link) VALUES ('" + a + "') ");
+            komut.ExecuteNonQuery();
+            con.Close();*/
+            
+        }
+        private void contentGonder(String a)
+        {
+            a = a.Replace("&#8217;", " ");
+            a = a.Replace("&quot;", "");
+            a = a.Replace("&#39;", " ");
+            a = a.Replace("&#160;", " ");
+            content[l] = a;
+            l++;
+            /*con.Open();
+            OleDbCommand komut = new OleDbCommand();
+            komut.Connection = con;
+            komut.CommandText = ("INSERT INTO haber(content) VALUES ('" + a + "') ");
+            komut.ExecuteNonQuery();
+            con.Close();*/
+        }
+
+        private void titleGonder(String a)
+        {
+            a = a.Replace("&quot;", "");
+            a = a.Replace("&#39;", " ");
+            a = a.Replace("&#160;", " ");
+            title[m] = a;
+            m++;
+            /*con.Open();
+            OleDbCommand komut = new OleDbCommand();
+            komut.Connection = con;
+            komut.CommandText = ("INSERT INTO haber(title) VALUES ('" + a + "') ");
+            komut.ExecuteNonQuery();
+            con.Close();*/
+        }
+        private void categoryGonder(String a)
+        {
+            a = a.Replace("&#39;", " ");
+            a = a.Replace("&#160;", " ");
+            category[n] = a;
+            n++;
+            /*con.Open(); 
+            OleDbCommand komut = new OleDbCommand();
+            komut.Connection = con;
+            komut.CommandText = ("INSERT INTO haber(category) VALUES ('" + a + "') ");
+            komut.ExecuteNonQuery();
+            con.Close();*/
+        }
+        
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void haberal_Click(object sender, EventArgs e)
+        {
+
+            
+
+            for (int i = 1; i < 5; i++)
+            {
+                Uri url = new Uri("https://www.ntv.com.tr/son-dakika/?sayfa="+ i);
+                WebClient client = new WebClient();
+                client.Encoding = Encoding.UTF8;
+                string html = client.DownloadString(url);
+                HtmlAgilityPack.HtmlDocument hdoc = new HtmlAgilityPack.HtmlDocument();
+                hdoc.LoadHtml(html);
+                HtmlNodeCollection haber = hdoc.DocumentNode.SelectNodes("//h2[@class='title']");
+                HtmlNodeCollection link = hdoc.DocumentNode.SelectNodes("//a[@href]");
+                
+                linkBox.Items.Add("----------------------------" + i + ".Sayfa Haber Linkleri ----------------------------");
+                int index = 1;
+                int index2 = 1;
+                int index3 = 1;
+                foreach (var haberGet in link)
+                {
+
+                    string hrefValue = haberGet.GetAttributeValue("href", string.Empty);
+
+                    if (hrefValue.Length > 40 && hrefValue.IndexOf("Descending") == -1 && hrefValue.IndexOf("kralmuzik") == -1)
+                    {
+                        linkBox.Items.Add(i + ".Sayfa" + index++ + ". Haber Linki =  https://www.ntv.com.tr" + hrefValue);
+                        String newLink = "https://www.ntv.com.tr" + hrefValue;
+                        linkGonder(newLink);
+        
+
+                        Uri url1 = new Uri(newLink);
+                        
+                        WebClient client1 = new WebClient();
+                        client1.Encoding = Encoding.UTF8;
+                        string html1 = client1.DownloadString(url1);
+                        HtmlAgilityPack.HtmlDocument hdoc1 = new HtmlAgilityPack.HtmlDocument();
+                        hdoc1.LoadHtml(html1);
+                        HtmlNodeCollection haber1 = hdoc1.DocumentNode.SelectNodes("//a[@property='v:title']");
+                        HtmlNodeCollection habericerik = hdoc1.DocumentNode.SelectNodes("//meta[@property='og:description']");
+                        
+
+                        foreach (var haber2Get in habericerik)
+                        {
+                            String haberString1 = haber2Get.GetAttributeValue("content", string.Empty);
+                                newsBox.Items.Add("------------------------------------------");
+                                newsBox.Items.Add(i + ".Sayfa" + index3++ + ".Haber iceriği");
+                                newsBox.Items.Add("------------------------------------------");
+                                newsBox.Items.Add(haberString1);
+                                contentGonder(haberString1);
+                               
+                        }
+
+                        headBox.Items.Add("------------------------------------------");
+                        headBox.Items.Add(i + ".Sayfa" + index2++ + ".Haber Başlığı");
+                        headBox.Items.Add("------------------------------------------");
+
+                        foreach (var haber1Get in haber1)
+                        {
+                            headBox.Items.Add(haber1Get.InnerText);
+                            String category = haber1Get.InnerText;
+                            if (category != "Haber")
+                                categoryGonder(category);
+                        }
+                    }
+                } 
+                
+                haberler.Items.Add("----------------------------" + i + ".Sayfa Haberleri ----------------------------");
+
+                foreach (var haberGet in haber)
+                {
+                    haberler.Items.Add(haberGet.InnerText);
+                    titleGonder(haberGet.InnerText);
+                }   
+            }
+           
+        }
+
+        private void haberler_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dbKaydet_Click(object sender, EventArgs e)
+        {
+            gonderDb();
+        }
+
+        private void listHaber_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void veriGet_Click(object sender, EventArgs e)
+        {
+            verileriGetir();
+        }
+        
+        private void gonderDb()
+        {
+            for(int c=0; c<72;c++)
+            { 
+                 con.Open();
+                 OleDbCommand komut = new OleDbCommand();
+                 komut.Connection = con;
+                 komut.CommandText = ("INSERT INTO haber(content,link,category,title) VALUES ('" + content[c] + "','" + link[c] + "','" + category[c] + "','" + title[c] + "') ");
+                 komut.ExecuteNonQuery();
+                 con.Close();
+            }
+        }
+    }
+}
+//Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Sefa\Documents\haberdb.accdb;Persist Security Info=True
